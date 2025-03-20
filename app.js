e53505998f9829cc7394c7c2aadde0e960d6fd22
tet-kitamori <tetkita2021@outlook.jp>
2025-03-20 22:04:27 +0900

    Add session for users page

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// kita added for use session
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var helloRouter = require('./routes/hello');
+var helloRouter = require('./routes/hello');  // Kita added
 
 var app = express();
+
+// Kita add express-session setup
+const session_opt = {
+  secret: '** himitu-no-kagi **',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60 * 60 * 24 }
+}
+app.use(session(session_opt));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/hello', helloRouter);
+app.use('/hello', helloRouter);     // Kita added
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+    if (req.session.comments == undefined) {
+      req.session.comments = []
+    }
+    var data = {
+        title: 'Expresso',
+        greet: 'Hello!',
+        username: '',
+        content : '行楽シーズンです。あなたは何をしたいですか？',
+        comments: req.session.comments
+    };
+    res.render('users', data);
+});
+
+router.post('/post', (req, res, next) => {
+    var msg = req.body['message'];
+    var comment = req.body.uname + 'さん： ' + msg;
+    req.session.comments.unshift(comment);
+    var data = {
+        title: 'Expresso',
+        greet: 'Hello!',
+        username: req.body.uname,
+        content: req.body.uname + 'さんは「' + msg + '」と送信しました。',
+        comments: req.session.comments
+    };
+    res.render('users', data);
 });
 
 module.exports = router;
